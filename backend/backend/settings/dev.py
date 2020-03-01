b045acc733b26b269bd09ba93fd81bde706deee1
AleksaC <aleksacukovic1@gmail.com>
2020-03-02 00:18:59 +0100

    Fix settings

--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -1,3 +1,5 @@
+import os
+
 from .common import *
 
 SECRET_KEY = "5z!=r3pah3sdi1%3)@317*vnwdswra8lxyrz8175&q8z%6%z&a"
@@ -8,11 +10,11 @@ ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.contrib.gis.db.backends.postgis",
-        "NAME": "gis",
-        "USER": "aleksa",
-        "PASSWORD": "projekat",
-        "HOST": "db",
-        "PORT": "5432",
-    },
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.environ.get("DB_NAME", "gis"),
+        "USER": os.environ.get("DB_USER", "aleksa"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "projekat"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+    }
 }
--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -1,3 +1,5 @@
+import os
+
 from .common import *
 
 DEBUG = False
